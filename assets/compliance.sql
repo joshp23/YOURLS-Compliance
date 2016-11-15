63ffb6d01caeb229184e1cd5cf465c7503dd14b1
Josh Panter <joshu@unfettered.net>
2016-11-15 10:44:37 -0500

    Delete compliance.sql

--- assets/compliance.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `flagged` (
-  `keyword` varchar(200) NOT NULL,
-  `clicks` int(10) NOT NULL default '0',
-  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `reason` text,
-  `addr` varchar(200) default NULL,
-  PRIMARY KEY  (`keyword`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
